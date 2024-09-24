35c34e2fbacc6c797d5d147f43c340e72228e589
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2024-09-24 22:52:26 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/example_mod/ExampleMod.kt
@@ -0,0 +1,10 @@
+package com.example.example_mod
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(
+    modid = Tags.MOD_ID,
+    modLanguageAdapter = "io.github.chaosunity.forgelin.KotlinAdapter"
+)
+object ExampleMod {
+}
